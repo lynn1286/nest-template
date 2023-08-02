aa5ef59f6f2aeb8a57d72cecb1836a0106204e52
lynn1286 <lynn1286@126.com>
2023-08-02 23:00:13 +0800

    feat: :fire: 重构一些代码

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Bootstrap');
+
+logger.log(`程序开始启动, 当前环境： ${process.env.NODE_ENV ?? 'local'}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +18,7 @@ async function bootstrap() {
   };
 
   await app.listen(http.port);
+  logger.log(`应用程序已启动并侦听端口: ${http.port}`);
 }
 
 bootstrap();
